f6a87a03e9f0f883f8dff3cca9603fb604a4d050
Aliyor01 <127713784+Aliyor01@users.noreply.github.com>
2023-11-16 22:52:26 +0500

    Update task_1.cpp

--- src/task_1.cpp
+++ src/task_1.cpp
@@ -5,43 +5,23 @@
  */
 
 #include "task_1.h"
-#include <algorithm> // Include the algorithm header for swap
-#include <vector>
-#include <iostream> // Include the iostream header for output
+#include <algorithm>
+#include <iostream>
 
-using namespace std;
-
-// Function to perform Bubble Sort on a vector of strings
-void bubbleSort(vector<string> &books) {
+void bubbleSort(std::vector<std::string> &books) {
     int n = books.size();
 
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
-            // Compare adjacent book titles and swap if they are in the wrong order
             if (books[j] > books[j + 1]) {
-                swap(books[j], books[j + 1]);
+                std::swap(books[j], books[j + 1]);
             }
         }
     }
 }
 
-// Function to display the sorted vector of strings
-void displaySortedBooks(const vector<string> &books) {
+void displaySortedBooks(const std::vector<std::string> &books) {
     for (const auto &book : books) {
-        cout << book << endl;
+        std::cout << book << std::endl;
     }
 }
-
-int main() {
-    // Example usage
-    vector<string> books = {"C Programming", "Java Basics", "Python Mastery", "Algorithm Design"};
-    
-    // Sort the vector of strings using Bubble Sort
-    bubbleSort(books);
-
-    // Display the sorted books
-    displaySortedBooks(books);
-
-    return 0;
-}
-
