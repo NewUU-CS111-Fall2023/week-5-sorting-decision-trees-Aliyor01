3cbb641076726e820f1be03123753e05526d5e13
Aliyor01 <127713784+Aliyor01@users.noreply.github.com>
2023-11-16 22:32:07 +0500

    Update task_2.h

--- src/task_2.h
+++ src/task_2.h
@@ -1,1 +1,14 @@
+/*
+ * Author: Ergashev Aliyorjon
+ * Date: 16.11.2023
+ * Name: main.cpp
+ */
 
+#ifndef TASK_2_H
+#define TASK_2_H
+
+#include <vector>
+
+void selectionSort(std::vector<int> &denominations);
+
+#endif
