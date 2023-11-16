d8a77998dae33c0806f826d323dc87bb4a207558
Aliyor01 <127713784+Aliyor01@users.noreply.github.com>
2023-11-16 22:49:18 +0500

    Update task_5.h

--- src/task_5.h
+++ src/task_5.h
@@ -1,1 +1,14 @@
+/*
+ * Author: Ergashev Aliyorjon
+ * Date: 16.11.2023
+ * Name: main.cpp
+ */
 
+#ifndef TASK_5_H
+#define TASK_5_H
+
+#include <vector>
+
+void displayColors(const std::vector<std::vector<char>> &matrix);
+
+#endif // TASK_5_H
