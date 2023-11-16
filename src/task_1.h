6a937be84e924ce05e20683a93dcfff7e462d1f8
Aliyor01 <127713784+Aliyor01@users.noreply.github.com>
2023-11-16 22:27:48 +0500

    Update task_1.h

--- src/task_1.h
+++ src/task_1.h
@@ -1,5 +1,15 @@
 /*
- * Author:
- * Date:
- * Name:
+ * Author: Ergashev Aliyorjon
+ * Date: 16.11.2023
+ * Name: main.cpp
  */
+
+#ifndef TASK_1_H
+#define TASK_1_H
+
+#include <vector>
+#include <string>
+
+void bubbleSort(std::vector<std::string> &books);
+
+#endif
