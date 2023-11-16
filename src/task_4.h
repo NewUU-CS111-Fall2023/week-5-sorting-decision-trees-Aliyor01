f9700cf7a19046a83f238c6448694885a45c9a34
Aliyor01 <127713784+Aliyor01@users.noreply.github.com>
2023-11-16 22:43:04 +0500

    Update task_4.h

--- src/task_4.h
+++ src/task_4.h
@@ -1,1 +1,12 @@
+/*
+ * Author: Ergashev Aliyorjon
+ * Date: 16.11.2023
+ * Name: main.cpp
+ */
 
+#ifndef TASK_4_H
+#define TASK_4_H
+
+int countElementsWithSmallerNeighbors(int arr[], int size);
+
+#endif // TASK_4_H
