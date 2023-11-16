a9e18a9937b87abd92e9fec2df79ab75235b41c0
Aliyor01 <127713784+Aliyor01@users.noreply.github.com>
2023-11-16 22:44:03 +0500

    Update task_4.cpp

--- src/task_4.cpp
+++ src/task_4.cpp
@@ -4,14 +4,12 @@
  * Name: main.cpp
  */
 
-#include <iostream>
+#include "task_4.h"
 
-// Function to count elements where both neighboring elements are smaller
 int countElementsWithSmallerNeighbors(int arr[], int size) {
     int count = 0;
 
     for (int i = 1; i < size - 1; ++i) {
-        // Check if the current element is greater than both neighboring elements
         if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
             count++;
         }
@@ -19,18 +17,3 @@ int countElementsWithSmallerNeighbors(int arr[], int size) {
 
     return count;
 }
-
-int main() {
-    // Example usage
-    int arr[] = {5, 2, 8, 1, 4, 6};
-    int size = sizeof(arr) / sizeof(arr[0]);
-
-    // Count elements where both neighboring elements are smaller
-    int result = countElementsWithSmallerNeighbors(arr, size);
-
-    // Display the result
-    std::cout << "Count of elements with both smaller neighbors: " << result << std::endl;
-
-    return 0;
-}
-
