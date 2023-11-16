081c4211a1a5a3e2cfe56b1b42f75d66a54ced2a
Aliyor01 <127713784+Aliyor01@users.noreply.github.com>
2023-11-16 22:54:10 +0500

    Update task_2.cpp

--- src/task_2.cpp
+++ src/task_2.cpp
@@ -5,42 +5,20 @@
  */
 
 
-#include <iostream>
-#include <vector>
-#include <algorithm> // Include the algorithm header for swap
+#include "task_2.h"
+#include <algorithm>
 
-using namespace std;
-
-void selectionSort(vector<int> &denominations) {
+void selectionSort(std::vector<int> &denominations) {
     int n = denominations.size();
 
     for (int i = 0; i < n - 1; i++) {
         int minIndex = i;
         for (int j = i + 1; j < n; j++) {
-            // Find the index of the minimum element in the remaining unsorted part
             if (denominations[j] < denominations[minIndex]) {
                 minIndex = j;
             }
         }
 
-        // Swap the found minimum element with the first element of the unsorted part
-        swap(denominations[i], denominations[minIndex]);
+        std::swap(denominations[i], denominations[minIndex]);
     }
 }
-
-int main() {
-    // Example usage
-    vector<int> denominations = {99, 19, 11};
-
-    // Sort the vector of denominations using Selection Sort
-    selectionSort(denominations);
-
-    // Display the sorted list of denominations
-    cout << "Sorted list of denominations:" << endl;
-    for (const int &denomination : denominations) {
-        cout << denomination << " ";
-    }
-
-    return 0;
-}
-
