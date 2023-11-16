bf4f624bf505089cecca71e138e7f75a698757ea
Aliyor01 <127713784+Aliyor01@users.noreply.github.com>
2023-11-16 22:56:51 +0500

    Update task_3.cpp

--- src/task_3.cpp
+++ src/task_3.cpp
@@ -4,32 +4,14 @@
  * Name: main.cpp
  */
 
-#include <iostream>
-#include <vector>
-#include <algorithm> // Include the algorithm header for swap
+#include "task_3.h"
+#include <algorithm>
 
 void rearrangeSoldiers(std::vector<std::string> &soldiers) {
     int n = soldiers.size();
 
     for (int i = 0; i < n - 1; i += 2) {
-        // Swap adjacent soldiers
         std::swap(soldiers[i], soldiers[i + 1]);
     }
-}
-
-int main() {
-    // Example usage
-    std::vector<std::string> soldiers = {"Alpha", "Bravo", "Charlie", "Delta", "Echo"};
-
-    // Rearrange the list of soldiers
-    rearrangeSoldiers(soldiers);
-
-    // Display the rearranged list of soldiers
-    std::cout << "Rearranged list of soldiers:" << std::endl;
-    for (const std::string &soldier : soldiers) {
-        std::cout << soldier << std::endl;
-    }
-
-    return 0;
 }
 
